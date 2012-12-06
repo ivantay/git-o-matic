cb6bd8ad0f8a039d0eb0bb30440df573a796606c
i.tay <i.tay@batc.i2r.a-star.edu.sg>
2012-12-06 17:17:00 +0800

    Support #29: SCM Wiki: Beautify code and make some more printouts.

--- src/sg/edu/astar/i2r/batc/common/HelloWorld.java
+++ src/sg/edu/astar/i2r/batc/common/HelloWorld.java
@@ -13,11 +13,11 @@ public class HelloWorld{
 
 	/**
 	 * The entry function of Hello World.
-	 * 
 	 * @param args
 	 * 	Not used.
 	 */
 	public static void main(String[] args){
+		System.out.println("Can there be a more boring Hello World?");
 		if((args!=null) && (args.length>0)){
 			System.out.println("Hello Again World");
 		}
